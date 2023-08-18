6b27f8798ef1d1265e711dbf60fce5b116e72f1b
22A91A04M7 <119720631+22A91A04M7@users.noreply.github.com>
2023-08-18 15:44:50 +0530

    n1,n2,n3,n4,n5=map(int,input().split()) per=(n1+n2+n3+n4+n5)/5 if per>=90:     print("Grade A") elif per>=80:     print("Grade B") elif per>=70:     print("Grade C") elif per>=60:     print("Grade D") elif per>=40:     print("Grade E") else:     print("Grade F")

---
+++ Grades.py
@@ -0,0 +1,14 @@
+n1,n2,n3,n4,n5=map(int,input().split())
+per=(n1+n2+n3+n4+n5)/5
+if per>=90:
+    print("Grade A")
+elif per>=80:
+    print("Grade B")
+elif per>=70:
+    print("Grade C")
+elif per>=60:
+    print("Grade D")
+elif per>=40:
+    print("Grade E")
+else:
+    print("Grade F")
